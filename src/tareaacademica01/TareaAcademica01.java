835c8764ba7b4101e6f8ff41eccfdd47281751d9
Angel Chira <125518726+AngelChira@users.noreply.github.com>
2023-09-13 16:11:27 -0500

    Agregando algunas validaciones con condicionales.

--- src/tareaacademica01/TareaAcademica01.java
+++ src/tareaacademica01/TareaAcademica01.java
@@ -10,31 +10,35 @@ public class TareaAcademica01 {
         final int MONTO_FIJO = UIT * 7;
         System.out.print("Ingrese el salario anual obtenido: ");
         float salarioAnual = teclado.nextFloat();
-        float remuneracionNetaAnual = (float) (salarioAnual - MONTO_FIJO);
-        float impuestoRenta = 0;
-        if (remuneracionNetaAnual > 0){
-            if (remuneracionNetaAnual <= (5*UIT)){
-                impuestoRenta = (float) (remuneracionNetaAnual * 0.08);
-            } else {
-                if (remuneracionNetaAnual <= (20*UIT)){
-                    impuestoRenta = (float) (remuneracionNetaAnual * 0.14);
+        if (salarioAnual <= 0){
+            System.out.println("Error, debe ingresar un salario anual mayor a 0.");
+        } else {
+            float remuneracionNetaAnual = (float) (salarioAnual - MONTO_FIJO);
+            float impuestoRenta = 0;
+            if (remuneracionNetaAnual > 0){
+                if (remuneracionNetaAnual <= (5*UIT)){
+                    impuestoRenta = (float) (remuneracionNetaAnual * 0.08);
                 } else {
-                    if (remuneracionNetaAnual <= (35*UIT)){
-                        impuestoRenta = (float) (remuneracionNetaAnual * 0.17);
+                    if (remuneracionNetaAnual <= (20*UIT)){
+                        impuestoRenta = (float) (remuneracionNetaAnual * 0.14);
                     } else {
-                        if (remuneracionNetaAnual <= (45*UIT)){
-                            impuestoRenta = (float) (remuneracionNetaAnual * 0.2);
+                        if (remuneracionNetaAnual <= (35*UIT)){
+                            impuestoRenta = (float) (remuneracionNetaAnual * 0.17);
                         } else {
-                            impuestoRenta = (float) (remuneracionNetaAnual * 0.3);
+                            if (remuneracionNetaAnual <= (45*UIT)){
+                                impuestoRenta = (float) (remuneracionNetaAnual * 0.2);
+                            } else {
+                                impuestoRenta = (float) (remuneracionNetaAnual * 0.3);
+                            }
                         }
                     }
                 }
+            } else {
+                System.out.printf("Usted no pagará nada ya que el salario anual es menor que el monto fijo que equivale a S/%d soles.%n",MONTO_FIJO);
             }
-        } else {
-            System.out.println("Usted no pagará nada ya que el salario anual es menor que el monto fijo.");
-        }
-        if (impuestoRenta > 0){
-            System.out.printf("El impuesto a la renta a pagar es S/%.2f soles. %n",impuestoRenta);
+            if (impuestoRenta > 0){
+                System.out.printf("El impuesto a la renta a pagar es S/%.2f soles. %n",impuestoRenta);
+            }
         }
     }
     
